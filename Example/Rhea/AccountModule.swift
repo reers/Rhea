ddfc35f8b31c090064a566188f47c579a4007aba
Asura19 <x.rhythm@qq.com>
2023-06-29 02:03:01 +0800

    eg: update example app

---
+++ Example/Rhea/AccountModule.swift
@@ -0,0 +1,32 @@
+//
+//  AccountModule.swift
+//  Rhea_Example
+//
+//  Created by phoenix on 2023/6/29.
+//  Copyright © 2023 Reer. All rights reserved.
+//
+
+import Foundation
+import RheaTime
+
+extension RheaEvent {
+    static let userLoggedIn: RheaEvent = "userLoggedIn"
+}
+
+@objc(REAccountModule)
+class AccountModule: NSObject, RheaDelegate {
+    static func rheaLoad() {
+        print("AccountModule \(#function)")
+    }
+
+    static func rheaAppDidFinishLaunching(context: RheaContext) {
+        print("AccountModule \(#function)")
+        print(context)
+    }
+
+    static func rheaDidReceiveCustomEvent(event: RheaEvent) {
+        if event == .userLoggedIn {
+            print(RheaEvent.userLoggedIn)
+        }
+    }
+}
