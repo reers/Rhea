c84bd02089c3932b2e7659e5be8d2204640cfb5c
Phoenix <x.rhythm@qq.com>
2024-08-21 19:24:58 +0800

    add macro

--- Package.swift
+++ Package.swift
@@ -2,26 +2,42 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "RheaTime",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
+        .macOS(.v10_15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RheaTime",
-            targets: ["RheaTime"]),
+            targets: ["RheaTime"])
+    ],
+    dependencies: [
+        // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        // Macro implementation that performs the source transformation of a macro.
+        .macro(
+            name: "RheaTimeMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
         .target(
             name: "RheaTime",
+            dependencies: ["RheaTimeMacros"],
             path: "Sources/RheaTime",
             swiftSettings: [.enableExperimentalFeature("SymbolLinkageMarkers")]
         ),
+        
         .target(name: "OCRhea", dependencies: ["RheaTime"]),
     ]
 )
