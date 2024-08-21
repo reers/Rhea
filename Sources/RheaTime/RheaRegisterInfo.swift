440d29d00218566664ef8894789d44e1c5d6cd61
Phoenix <x.rhythm@qq.com>
2024-08-21 18:28:10 +0800

    feat: add RheaFunction

--- Sources/RheaTime/RheaRegisterInfo.swift
+++ Sources/RheaTime/RheaRegisterInfo.swift
@@ -5,7 +5,9 @@
 //  Created by phoenix on 2024/6/19.
 //
 
+public typealias RheaFunction = @convention(c)(RheaContext) -> Void
+
 /// The `StaticString` is like `"rhea.load.5.true"`, that means `"$prefix.$rheaTimeName.$priority.$isRepeatable"`
-public typealias RheaRegisterInfo = (StaticString, @convention(c) (RheaContext) -> Void)
+public typealias RheaRegisterInfo = (StaticString, RheaFunction)
 
 
