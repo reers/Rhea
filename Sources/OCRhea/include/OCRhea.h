1dd6676ad913d1146ea69e45d98c1539766726de
Phoenix <x.rhythm@qq.com>
2024-08-30 15:41:25 +0800

    删除无用代码

--- Sources/OCRhea/include/OCRhea.h
+++ Sources/OCRhea/include/OCRhea.h
@@ -10,7 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface OCRhea : NSObject
-+ (void)keep;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Sources/RheaTime/Macros/Macros.swift
+++ Sources/RheaTime/Macros/Macros.swift
@@ -5,38 +5,10 @@
 //  Created by phoenix on 2024/8/21.
 //
 
-@freestanding(expression)
+@freestanding(declaration)
 public macro rhea(
     time: RheaEvent,
-    priority: Int,
-    repeatable: Bool,
-    function: RheaFunction
-) = #externalMacro(module: "RheaTimeMacros", type: "WriteSectionMacro")
-
-
-@freestanding(expression)
-public macro rhea2(
-    time: RheaEvent,
-    priority: Int,
-    repeatable: Bool,
-    function: RheaFunction
-) = #externalMacro(module: "RheaTimeMacros", type: "WriteSectionMacro2")
-
-@freestanding(declaration)
-public macro rhea3(
-    time: RheaEvent,
-    priority: Int = 100,
+    priority: Int = 5,
     repeatable: Bool = false,
     function: RheaFunction
-) = #externalMacro(module: "RheaTimeMacros", type: "WriteSectionMacro3")
-
-@freestanding(declaration)
-public macro rhea4(
-    time: RheaEvent,
-    priority: Int,
-    repeatable: Bool
-) = #externalMacro(module: "RheaTimeMacros", type: "WriteSectionMacro4")
-
-
-@freestanding(declaration)
-public macro routeHost(_ host: String) = #externalMacro(module: "RheaTimeMacros", type: "RouteHostMacro")
+) = #externalMacro(module: "RheaTimeMacros", type: "WriteSectionMacro")
