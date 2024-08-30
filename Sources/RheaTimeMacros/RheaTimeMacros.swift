3cb63faecf64b215139903aa49108ac2caa19cfd
Phoenix <x.rhythm@qq.com>
2024-08-30 15:41:25 +0800

    删除无用代码

--- Sources/RheaTimeMacros/RheaTimeMacros.swift
+++ Sources/RheaTimeMacros/RheaTimeMacros.swift
@@ -3,18 +3,17 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
-
 public enum MacroExpansionError: Error {
     case invalidArguments
     case test(String)
 }
 
-public struct WriteSectionMacro: ExpressionMacro {
+public struct WriteSectionMacro: DeclarationMacro {
     
     public static func expansion(
         of node: some SwiftSyntax.FreestandingMacroExpansionSyntax,
         in context: some SwiftSyntaxMacros.MacroExpansionContext
-    ) throws -> SwiftSyntax.ExprSyntax {
+    ) throws -> [DeclSyntax] {
         let argumentList = node.argumentList
         var time: String = ""
         var priority: String = ""
@@ -30,7 +29,7 @@ public struct WriteSectionMacro: ExpressionMacro {
                 }
             case "priority":
                 if let intLiteral = argument.expression.as(IntegerLiteralExprSyntax.self) {
-                    priority = intLiteral.literal.text
+                    priority = intLiteral.digits.text
                 }
             case "repeatable":
                 if let boolLiteral = argument.expression.as(BooleanLiteralExprSyntax.self) {
@@ -38,7 +37,7 @@ public struct WriteSectionMacro: ExpressionMacro {
                 }
             case "function":
                 if let closureExpr = argument.expression.as(ClosureExprSyntax.self) {
-                    functionBody = closureExpr.statements.description
+                    functionBody = closureExpr.statements.description.trimmingCharacters(in: .whitespacesAndNewlines)
                     if let sig = closureExpr.signature {
                         signature = sig.description
                     }
@@ -50,185 +49,24 @@ public struct WriteSectionMacro: ExpressionMacro {
         
         let infoName = "\(context.makeUniqueName("rhea"))"
         
-        let expansionString = """
-            @_used @_section("__DATA,__rheatime") var \(infoName): RheaRegisterInfo = ("rhea.\(time).\(priority).\(repeatable)", { \(signature ?? "context in") \(functionBody) })
-            """
-        return ExprSyntax(stringLiteral: expansionString)
-    }
-}
-
-public struct WriteSectionMacro2: ExpressionMacro {
-    public static func expansion(
-        of node: some SwiftSyntax.FreestandingMacroExpansionSyntax,
-        in context: some SwiftSyntaxMacros.MacroExpansionContext
-    ) throws -> SwiftSyntax.ExprSyntax {
-        let argumentList = node.argumentList
-        var time: String = ""
-        var priority: String = ""
-        var repeatable: String = ""
-        
-        for argument in argumentList {
-            switch argument.label?.text {
-            case "time":
-                if let memberAccess = argument.expression.as(MemberAccessExprSyntax.self) {
-                    time = memberAccess.declName.baseName.text
-                }
-            case "priority":
-                if let intLiteral = argument.expression.as(IntegerLiteralExprSyntax.self) {
-                    priority = intLiteral.literal.text
-                }
-            case "repeatable":
-                if let boolLiteral = argument.expression.as(BooleanLiteralExprSyntax.self) {
-                    repeatable = boolLiteral.literal.text
-                }
-            default:
-                break
-            }
-        }
-        
-        let infoName = "\(context.makeUniqueName("rhea"))"
-        let funcName = "\(context.makeUniqueName("func"))"
-        
-        let entireCall = node.parent?.as(ExprSyntax.self) ?? node.as(ExprSyntax.self)!
-        let entireString = "\(entireCall)"
-        let closure = extractFromFirstBrace(entireString) ?? ""
-        
-        let expansionString = """
-            @_used
+        let declarationString = """
+            @_used 
             @_section("__DATA,__rheatime")
-            var \(infoName): RheaRegisterInfo = ("rhea.\(time).\(priority).\(repeatable)", \(funcName))
-            let \(funcName): RheaFunction = \(closure)
+            let \(infoName): RheaRegisterInfo = (
+                "rhea.\(time).\(priority).\(repeatable)",
+                { \(signature ?? "context in")
+                    \(functionBody)
+                }
+            )
             """
-        return ExprSyntax(stringLiteral: expansionString)
-    }
-    
-    static func extractFromFirstBrace(_ input: String) -> String? {
-        if let range = input.range(of: "{") {
-            return String(input[range.lowerBound...])
-        }
-        return nil
-    }
-}
-
-public struct WriteSectionMacro3: DeclarationMacro {
-    
-    public static func expansion(
-        of node: some SwiftSyntax.FreestandingMacroExpansionSyntax,
-        in context: some SwiftSyntaxMacros.MacroExpansionContext
-    ) throws -> [DeclSyntax] {
-        let argumentList = node.argumentList
-        var time: String = ""
-        var priority: String = ""
-        var repeatable: String = ""
-        var functionBody: String = ""
-        var signature: String?
-        
-        for argument in argumentList {
-            switch argument.label?.text {
-            case "time":
-                if let memberAccess = argument.expression.as(MemberAccessExprSyntax.self) {
-                    time = memberAccess.declName.baseName.text
-                }
-            case "priority":
-                if let intLiteral = argument.expression.as(IntegerLiteralExprSyntax.self) {
-                    priority = intLiteral.digits.text
-                }
-            case "repeatable":
-                if let boolLiteral = argument.expression.as(BooleanLiteralExprSyntax.self) {
-                    repeatable = boolLiteral.literal.text
-                }
-            case "function":
-                if let closureExpr = argument.expression.as(ClosureExprSyntax.self) {
-                    functionBody = closureExpr.statements.description
-                    if let sig = closureExpr.signature {
-                        signature = sig.description
-                    }
-                }
-            default:
-                break
-            }
-        }
-        
-        let infoName = "\(context.makeUniqueName("rhea"))"
-        
-        let declarationString = """
-            @_used @_section("__DATA,__rheatime") let \(infoName): RheaRegisterInfo = ("rhea.\(time).\(priority).\(repeatable)", { \(signature ?? "context in") \(functionBody) })
-            """
-        
         return [DeclSyntax(stringLiteral: declarationString)]
     }
 }
 
-public struct WriteSectionMacro4: DeclarationMacro {
-    
-    public static func expansion(
-        of node: some SwiftSyntax.FreestandingMacroExpansionSyntax,
-        in context: some SwiftSyntaxMacros.MacroExpansionContext
-    ) throws -> [DeclSyntax] {
-        let argumentList = node.argumentList
-        var time: String = ""
-        var priority: String = ""
-        var repeatable: String = ""
-        var functionBody: String = ""
-        var signature: String?
-        
-        for argument in argumentList {
-            switch argument.label?.text {
-            case "time":
-                if let memberAccess = argument.expression.as(MemberAccessExprSyntax.self) {
-                    time = memberAccess.declName.baseName.text
-                }
-            case "priority":
-                if let intLiteral = argument.expression.as(IntegerLiteralExprSyntax.self) {
-                    priority = intLiteral.digits.text
-                }
-            case "repeatable":
-                if let boolLiteral = argument.expression.as(BooleanLiteralExprSyntax.self) {
-                    repeatable = boolLiteral.literal.text
-                }
-            default:
-                break
-            }
-        }
-        
-        let infoName = "\(context.makeUniqueName("rhea"))"
-        
-        let declarationString = """
-            @_used @_section("__DATA,__rheatime") let \(infoName): StaticString = "rhea.\(time).\(priority).\(repeatable)"
-            """
-        
-        return [DeclSyntax(stringLiteral: declarationString)]
-    }
-}
-
-public struct RouteHostMacro: DeclarationMacro {
-    
-    public static func expansion(
-        of node: some FreestandingMacroExpansionSyntax,
-        in context: some MacroExpansionContext
-    ) throws -> [DeclSyntax] {
-        guard let argument = node.argumentList.first?.expression,
-              let stringLiteral = argument.as(StringLiteralExprSyntax.self),
-              let hostValue = stringLiteral.segments.first?.as(StringSegmentSyntax.self)?.content.text
-        else {
-            throw MacroExpansionError.invalidArguments
-        }
-        let name = context.makeUniqueName("routerHost")
-        let extensionDecl: DeclSyntax = """
-        @_used @_section("__DATA,__routable_host") let \(name): StaticString = "\(raw: hostValue)"
-        """
-
-        return [extensionDecl]
-    }
-}
 
 @main
 struct RheaTimePlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-        WriteSectionMacro.self,
-        WriteSectionMacro2.self,
-        WriteSectionMacro3.self,
-        WriteSectionMacro4.self,
-        RouteHostMacro.self
+        WriteSectionMacro.self
     ]
 }
