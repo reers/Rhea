31ed44c78176b4d8edca31d4fc0e5e1f9c58b73f
phoenix <x.rhythm@qq.com>
2026-04-04 01:18:22 +0800

    feat: update for Swift 6.3 - use stable @section and @used attributes
    - Replace experimental @_section/@_used with stable @section/@used
    - Remove SymbolLinkageMarkers experimental feature flag (now stable in Swift 6.1+)
    - Update Package.swift swift-tools-version to 6.1
    - Update README to require Xcode 26.4+ (Swift 6.1+)
    - Note: Users on older Xcode (16.0-26.3) should use version 2.2.8

--- RheaDemo/Package.swift
+++ RheaDemo/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,7 +21,7 @@ let package = Package(
             dependencies: [
                 .product(name: "RheaTime", package: "RheaTime") // 正确指定产品依赖
             ],
-            swiftSettings: [.enableExperimentalFeature("SymbolLinkageMarkers")]
+            swiftSettings: []
         ),
     ]
 )
